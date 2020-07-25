2897eaafa76e213aaa15018aa713e87168e96aa7
Mukesh Patel <patelmukesh325@gmail.com>
2020-07-25 21:59:43 +0530

    maven commit

---
+++ MavenBoot/src/main/java/com/example/demo/MavenBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenBootApplication.class, args);
+	}
+
+}
