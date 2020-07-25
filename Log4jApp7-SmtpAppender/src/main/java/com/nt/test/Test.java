b5a9c65dc87ee424ee8c0eb669b0b9d67eeee636
Mukesh Patel <patelmukesh325@gmail.com>
2020-07-25 22:29:26 +0530

    log4j commit

--- Log4jApp7-SmtpAppender/src/main/java/com/nt/test/Test.java
+++ Log4jApp7-SmtpAppender/src/main/java/com/nt/test/Test.java
@@ -5,7 +5,7 @@ import org.apache.log4j.Logger;
 public class Test {
 	private static Logger logger=Logger.getLogger(Test.class);
 	public static void main(String[] args) {
-		
+		//Log4j Properties
          logger.debug("Mukesh");
          logger.info("From");
          logger.warn("Mariahu");
