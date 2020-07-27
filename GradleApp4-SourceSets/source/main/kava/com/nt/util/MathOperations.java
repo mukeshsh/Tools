c8107d9a674842714b2c9261af376e9d7ff5ae45
Mukesh Patel <patelmukesh325@gmail.com>
2020-07-27 12:44:57 +0530

    output through sourceSet

--- GradleApp4-SourceSets/source/main/kava/com/nt/util/MathOperations.java
+++ GradleApp4-SourceSets/source/main/kava/com/nt/util/MathOperations.java
@@ -1,13 +1,22 @@
 package com.nt.util;
 
+import java.util.Scanner;
+
 public class MathOperations {
 	public int sum(int a,int b) {
 		return a+b;
 	}
       public static void main(String[] args) {
 		MathOperations operations=null;
+		Scanner sc=null;
+		
 		operations=new MathOperations();
 		System.out.println("Result is ::"+operations.sum(100, 300));
+		//read inputs
+		sc=new Scanner(System.in);
+		System.out.println("Enter Name::");
+		String name=sc.next();
+		System.out.println("Name::"+name);
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			System.out.println("driver loaded..........");
